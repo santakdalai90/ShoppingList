709c76c4c297673d55fee045226e7f776a6cb6e7
Sandhya rani <thippaluri.sandhya@gmail.com>
2021-12-20 07:45:40 +0530

    This is msg is to know that i have changed my port

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-    //"fmt"
+	//"fmt"
 	//"io"
 	"net/http"
 	"os"
-    //"github.com/spf13/viper"
-    //"strconv"
+
+	//"github.com/spf13/viper"
+	//"strconv"
 	"github.com/gin-contrib/logger"
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -14,17 +15,17 @@ import (
 )
 
 func main() {
-   // Log as JSON instead of the default ASCII formatter.
-  log.SetFormatter(&log.JSONFormatter{})
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
 
-  // Output to stdout instead of the default stderr
-  // Can be any io.Writer, see below for File example
-  log.SetOutput(os.Stdout)
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
 
-  // Only log the warning severity or above.
-  log.SetLevel(log.DebugLevel)
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
 
-    router := gin.New()
+	router := gin.New()
 
 	// Add the logger middleware
 	router.Use(logger.SetLogger())
@@ -32,12 +33,14 @@ func main() {
 	router.GET("/ping", func(c *gin.Context) {
 		log.Info("Received ping message")
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"attribute": "salary",
 		})
 	})
 
+	//router.POST()
 	//routes.InitRoutes(router)
 
-	log.Info("Port", "8888", "Starting web server")
+	log.Info("Port", "8000", "Starting web server")
+	router.Run(":8000")
 	//router.Run(fmt.Sprintf(":%s", strconv.Itoa(viper.GetInt("webserver.port"))))
 }
