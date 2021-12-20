0c4ac2aeb215d699739da9cfd9327fdae8b73c0a
sdalai_codeshark <sdalai@codeshark.in>
2021-12-20 07:37:12 +0530

    started using port 9595

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-    //"fmt"
+	//"fmt"
 	//"io"
 	"net/http"
 	"os"
-    //"github.com/spf13/viper"
-    //"strconv"
+
+	//"github.com/spf13/viper"
+	//"strconv"
 	"github.com/gin-contrib/logger"
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -14,17 +15,17 @@ import (
 )
 
 func main() {
-   // Log as JSON instead of the default ASCII formatter.
-  log.SetFormatter(&log.JSONFormatter{})
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
 
-  // Output to stdout instead of the default stderr
-  // Can be any io.Writer, see below for File example
-  log.SetOutput(os.Stdout)
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
 
-  // Only log the warning severity or above.
-  log.SetLevel(log.DebugLevel)
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
 
-    router := gin.New()
+	router := gin.New()
 
 	// Add the logger middleware
 	router.Use(logger.SetLogger())
@@ -38,6 +39,6 @@ func main() {
 
 	//routes.InitRoutes(router)
 
-	log.Info("Port", "8888", "Starting web server")
-	//router.Run(fmt.Sprintf(":%s", strconv.Itoa(viper.GetInt("webserver.port"))))
+	log.Info("Port", "9595", "Starting web server")
+	router.Run(":9595")
 }
