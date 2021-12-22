538623bbc6488fc7d71fad828ac7d55be99ed1ab
Maheswari1992 <maheswari214@gmail.com>
2021-12-23 01:04:26 +0530

    creat gorm to the project

---
+++ config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+func LoadConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath("./config")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error while reading config: %s", err.Error()))
+	}
+}
