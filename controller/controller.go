9adbc2231fe76e596bae4b7f7cbe0ce420a38924
sdalai_codeshark <sdalai@codeshark.in>
2022-01-07 21:40:59 +0530

    added changes to create a shopping list for a user

--- controller/controller.go
+++ controller/controller.go
@@ -7,12 +7,14 @@ import (
 )
 
 var (
-	db          *gorm.DB
-	userWrapper *wrapper.UserWrapper
+	db                  *gorm.DB
+	userWrapper         *wrapper.UserWrapper
+	shoppingListWrapper *wrapper.ShoppingListWrapper
 )
 
 // InitializeController initializes the controller with a DB as parameter
 func InitializeController(DB *gorm.DB) {
 	db = DB
 	userWrapper = wrapper.CreateUserWrapper(db)
+	shoppingListWrapper = wrapper.CreateShoppingListWrapper(db)
 }
