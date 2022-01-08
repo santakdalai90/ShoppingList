c009c562226a87dfbdb38ed8914a60d8019ec39c
prasanth127 <kudumala12@gmail.com>
2022-01-08 22:39:41 +0530

    added docker compose file

--- model/shopping_list.go
+++ model/shopping_list.go
@@ -6,5 +6,5 @@ type ShoppingList struct {
 	gorm.Model
 	Title string `gorm:"not null"`
 	Items []Item
-	Users []User `gorm:"many2many:user_shopping_lists;association_foreignkey:UserID"`
+	Users []User `gorm:"many2many:user_shopping_lists"`
 }
